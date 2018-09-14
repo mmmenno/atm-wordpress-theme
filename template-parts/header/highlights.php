254c16520d354379b2a21cb0a5b1fe365f5e1799
mmmenno <menno@denengelse.nl>
2018-09-14 11:06:59 +0200

    initial commit

---
+++ template-parts/header/highlights.php
@@ -0,0 +1,21 @@
+<div class="wrap">
+	<div style="margin-top: 30px;"></div>
+	<div id="highlights" class="content-area">
+		<div class="highlight" id="highlight1">
+			<img src="<?= get_theme_file_uri( '/assets/images/alida.png' ) ?>" />
+			<p><a href="">ALiDa Cloud</a>: linked datasets on Amsterdam history</p>
+		</div>
+		<div class="highlight" id="highlight3">
+			<a href="https://tiles.amsterdamtimemachine.nl">
+				<img src="<?= get_theme_file_uri( '/assets/images/3d.png' ) ?>" />
+			</a>
+			<p>3D: <a href="">reconstructing</a> Amsteram</p>
+		</div>
+		<div class="highlight" id="highlight2">
+			<a href="https://tiles.amsterdamtimemachine.nl">
+				<img src="<?= get_theme_file_uri( '/assets/images/maptiles.png' ) ?>" />
+			</a>
+			<p>Maps: <a href="https://tiles.amsterdamtimemachine.nl">go back in time</a>, use these tiles.</p>
+		</div>
+	</div>
+</div>
